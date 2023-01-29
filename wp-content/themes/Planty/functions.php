a69cf97c63e1d2359cd35bd3300f470e392c84f0
Audrey Diez <audrey.diez@gmail.com>
2023-01-29 23:43:47 +0100

    feat: enqueue styles

--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -4,5 +4,7 @@ add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 
 function theme_enqueue_styles(){
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    // Ajouter une autre feuille de style (exemple : du css compilé)
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/assets/css/main.css', array(), filemtime(get_stylesheet_directory() . '/assets/css/main.css'));
 }
 
--- wp-content/themes/Planty/header.php
+++ wp-content/themes/Planty/header.php
@@ -31,7 +31,7 @@
 		?>
 
 		<header id="site-header" class="header-footer-group">
-        <!-- <div class="test">LOL</div> -->
+            <div class="test">LOL</div>
 
 			<div class="header-inner section-inner">
 
