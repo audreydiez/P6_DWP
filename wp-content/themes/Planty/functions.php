1c1300f25acc72f583e806cc401061411d41db06
Audrey Diez <audrey.diez@gmail.com>
2023-02-12 00:45:25 +0100

    feat: form responsive

--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -51,11 +51,3 @@ register_nav_menus( array(
 
 
 
-// Remove span in contact form 7
-add_filter('wpcf7_form_elements', function($content) {
-    $content = preg_replace('/<(span).*?class="\s*(?:.*\s)?wpcf7-form-control-wrap(?:\s[^"]+)?\s*"[^\>]*>(.*)<\/\1>/i', '\2', $content);
-
-    $content = str_replace('<br />', '', $content);
-        
-    return $content;
-});
